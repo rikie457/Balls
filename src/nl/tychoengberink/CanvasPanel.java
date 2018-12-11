048ca95801a159c065e3d25899bb8db0664260c8
rikie457 <rikie456@gmail.com>
2018-12-11 23:05:15 +0100

    Added more info fields and updated Physics for faster processing

--- src/nl/tychoengberink/CanvasPanel.java
+++ src/nl/tychoengberink/CanvasPanel.java
@@ -9,6 +9,7 @@ public class CanvasPanel extends JPanel {
     private double maxheight;
     public ArrayList<Ball> balls = new ArrayList<Ball>();
     private Info info = new Info();
+    private Physics physics = new Physics();
 
     public CanvasPanel(double maxwidth, double maxheight) {
         this.maxwidth = maxwidth;
@@ -18,23 +19,30 @@ public class CanvasPanel extends JPanel {
     public void initialize() {
         double wh, mass;
         super.setBackground(Color.black);
+
         for (int i = 0; i < 200; i++) {
             wh = (Math.random() * 25) + 10;
             mass = ((wh / 2) *  (wh / 2)) * Math.PI;
             balls.add(new Ball((Math.random() * 1920), (Math.random() * 1080), wh, wh, new Color((int) (Math.random() * 0x1000000)), this.maxwidth, this.maxheight, (Math.random() * 2), (Math.random() * 2), mass));
         }
+        info.getLowestMass(balls);
+        info.getHighestMass(balls);
+        info.getBallCount(balls);
 
     }
 
 
     public void paint(Graphics g) {
+        Info.addToRefreshCounter();
+        Info.updateHighestSpeedX(balls);
+        Info.updateHighestSpeedY(balls);
         super.paint(g);
         info.draw(g);
         for (int i = 0; i < balls.size(); i++) {
             balls.get(i).draw(g);
 
             for (int j = 0; j < balls.size(); j++) {
-               Physics.checkCollisionBall(balls.get(i), balls);
+               physics.checkCollisionBall(balls.get(i), balls);
             }
 
         }
