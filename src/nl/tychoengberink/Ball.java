38fe4d23482484d6d58db94ae7cca8e0cf9cb084
rikie457 <rikie456@gmail.com>
2018-12-12 15:49:36 +0100

    Optimalisations

--- src/nl/tychoengberink/Ball.java
+++ src/nl/tychoengberink/Ball.java
@@ -1,6 +1,7 @@
 package nl.tychoengberink;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Ball {
     private double mass;
@@ -14,8 +15,9 @@ public class Ball {
     private double bx; //boundry X axle
     private double by; //boundry Y axle
     private Color color; //Color of Ball
+    private ArrayList<Ball> balls;
 
-    public Ball(double x, double y, double width, double height, Color color, double bx, double by, double speedY, double speedX, double mass) {
+    public Ball(double x, double y, double width, double height, Color color, double bx, double by, double speedY, double speedX, double mass, ArrayList<Ball> balls) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -27,9 +29,10 @@ public class Ball {
         this.bx = bx;
         this.by = by;
         this.mass = mass;
+        this.balls = balls;
     }
 
-    public Ball(double x, double y, double width, double height, Color color, double bx, double by, double mass) {
+    public Ball(double x, double y, double width, double height, Color color, double bx, double by, double mass, ArrayList<Ball> balls) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -41,13 +44,46 @@ public class Ball {
         this.bx = bx;
         this.by = by;
         this.mass = mass;
+        this.balls = balls;
+
+    }
+
+    public void checkCollisionBall( ArrayList<Ball> balls) {
+        for (int i = 0; i < balls.size(); i++) {
+            if (this == balls.get(i)) {
+                continue;
+            }
+            double xd = this.getX() - balls.get(i).getX();
+            double yd = this.getY() - balls.get(i).getY();
+            double distSqr = (xd * xd) + (yd * yd);
+            if (distSqr <= (this.getRadius() + balls.get(i).getRadius()) * (this.getRadius() + balls.get(i).getRadius())) {
+                Info.addToBallCollisionCounter();
+                double xVelocity = balls.get(i).getSpeedX() - this.getSpeedX();
+                double yVelocity = balls.get(i).getSpeedY() - this.getSpeedY();
+                double dotProduct = xd * xVelocity + yd * yVelocity;
+
+                if (dotProduct > 0) {
+                    double collisionScale = dotProduct / distSqr;
+                    double xCollision = xd * collisionScale;
+                    double yCollision = yd * collisionScale;
+                    double combinedMass = this.getMass() + balls.get(i).getMass();
+                    double collisionWeightA = 2 * balls.get(i).getMass() / combinedMass;
+                    double collisionWeightB = 2 * this.getMass() / combinedMass;
+                    this.speedX += collisionWeightA * xCollision;
+                    this.speedY += collisionWeightA * yCollision;
+                    balls.get(i).speedX -= collisionWeightB * xCollision;
+                    balls.get(i).speedY -= collisionWeightB * yCollision;
+
+                }
+            }
+        }
 
     }
 
     public void draw(Graphics g) {
 
-        x = x + speedX;
-        y = y + speedY;
+        this.x = x + speedX;
+        this.y = y + speedY;
 
         if (this.x - this.radius < 0) {
             this.speedX = -this.speedX;
@@ -71,7 +107,7 @@ public class Ball {
             // setColor(Color.red);
             this.y = this.by - this.radius;
         }
-
+        checkCollisionBall(balls);
         g.setColor(this.color);
         g.fillOval((int) this.x, (int) this.y, (int) this.width, (int) this.height);
 
--- src/nl/tychoengberink/CanvasPanel.java
+++ src/nl/tychoengberink/CanvasPanel.java
@@ -9,7 +9,6 @@ public class CanvasPanel extends JPanel {
     private double maxheight;
     public ArrayList<Ball> balls = new ArrayList<Ball>();
     private Info info = new Info();
-    private Physics physics = new Physics();
 
     public CanvasPanel(double maxwidth, double maxheight) {
         this.maxwidth = maxwidth;
@@ -20,10 +19,10 @@ public class CanvasPanel extends JPanel {
         double wh, mass;
         super.setBackground(Color.black);
 
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 1000; i++) {
             wh = (Math.random() * 25) + 10;
             mass = ((wh / 2) *  (wh / 2)) * Math.PI;
-            balls.add(new Ball((Math.random() * 1920), (Math.random() * 1080), wh, wh, new Color((int) (Math.random() * 0x1000000)), this.maxwidth, this.maxheight, (Math.random() * 2), (Math.random() * 2), mass));
+            balls.add(new Ball((Math.random() * 1920), (Math.random() * 1080), wh, wh, new Color((int) (Math.random() * 0x1000000)), this.maxwidth, this.maxheight, (Math.random() * 2), (Math.random() * 2), mass, balls));
         }
         info.getLowestMass(balls);
         info.getHighestMass(balls);
@@ -36,14 +35,12 @@ public class CanvasPanel extends JPanel {
         Info.addToRefreshCounter();
         Info.updateHighestSpeedX(balls);
         Info.updateHighestSpeedY(balls);
+
+
         super.paint(g);
         info.draw(g);
         for (int i = 0; i < balls.size(); i++) {
             balls.get(i).draw(g);
-
-            for (int j = 0; j < balls.size(); j++) {
-               physics.checkCollisionBall(balls.get(i), balls);
-            }
 
         }
 
